ff1ba43b395bfcb1200ddb9f1e96bc9900115ce8
Dmitry <wodi220500@gmail.com>
2020-09-21 21:01:53 +0300

    added Model

---
+++ MyFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  MyFollowers
+//
+//  Created by Dmitriy Chernov on 21.09.2020.
+//  Copyright © 2020 Dmitriy Chernov. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
